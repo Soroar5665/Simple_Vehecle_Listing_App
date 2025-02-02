49884554b950040100d5f732ac8f9841ad5da787
Soroar5665 <soroarjahan17@gmail.com>
2025-02-02 22:08:12 +0600

    final commit

--- app/classes/VehicleActions.php
+++ app/classes/VehicleActions.php
@@ -5,5 +5,5 @@ interface VehicleActions {
    public function addVehicle($data);
    public function editVehicle($id, $data);
    public function deleteVehicle($id);
-   public function getVehicle();
+   public function getVehicles();
 }
